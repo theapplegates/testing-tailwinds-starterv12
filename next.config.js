9b4fef73aff33bf371bdb5db4d9f36395cdc77b9
Paul <paul@theapplegates.com>
2021-10-28 07:17:59 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+module.exports = {
+  images: {
+    domains: ['assets.vercel.com'],
+    formats: ['image/avif', 'image/webp'],
+  },
+}
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
